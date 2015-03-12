ad2f0d3e4d806c55ec69ebd705beb2d8fa06bc5e
bradcurtis <bradcurtis74@gmail.com>
2015-03-12 09:14:17 -0500

    update

---
+++ sampleapp/DAL/FormContext.cs
@@ -0,0 +1,30 @@
+﻿using sampleapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace sampleapp.DAL
+{
+    public class FormContext: DbContext
+    {
+
+        public FormContext(): base("FormContext")
+        {
+        }
+        
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+
+        public DbSet<DataEntryForm> Forms { get; set; }
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
---
+++ sampleapp/Models/DataEntryForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sampleapp.Models
+{
+    public class DataEntryForm
+    {
+
+        public int ID { get; set; }
+        public virtual Person Person { get; set; }
+
+        public IList<SelectListItem> MyChoices { get; set; }  
+    }
+}
