b6b03ce12e34e2a89b74c39f5276b72cf64af666
bradcurtis <bradcurtis74@gmail.com>
2016-01-04 13:31:19 -0600

    initial update to ionic

--- IonicCordovaProject1/IonicCordovaProject1/www/js/birthday.service.js
+++ IonicCordovaProject1/IonicCordovaProject1/www/js/birthday.service.js
@@ -1,8 +1,8 @@
 (function() {
 
-    angular.module('starter.services').factory('BirthdayService', ['$q', 'Loki', BirthdayService]);
+    angular.module('starter.services').factory('BirthdayService', ['$q', 'Loki', '$rootScope', BirthdayService]);
         
-    function BirthdayService($q, Loki) {
+    function BirthdayService($q, Loki, $rootScope) {
         var _db;
         var _birthdays;
         var idbAdapter = new LokiIndexedAdapter('loki');
@@ -12,44 +12,56 @@
             _db = new Loki('birthdaysDB',
                     {
                         autosave: true,
+                       //autoload:true,
                         autosaveInterval: 1000, // 1 second
-                        adapter: idbAdapter
+                       //autoloadCallback: loadHandler,
+                       adapter: idbAdapter//fsAdapter adaptor for production deployment
                     });
+            
+            var options = {
+                birthdays: {
+                    proto: Object,
+                    inflate: function (src, dst) {
+                        var prop;
+                        for (prop in src) {
+                            if (prop === 'Date') {
+                                dst.Date = new Date(src.Date);
+                            } else {
+                                dst[prop] = src[prop];
+                            }
+                        }
+                    }
+                }
+            };
+            _db.loadDatabase(options, function () {
+                _birthdays = _db.getCollection('birthdays');
+                if (_birthdays) {
+                    _db.removeCollection('birthdays')
+                }
+
+               
+                    _birthdays = _db.addCollection('birthdays');
+                
+
+                
+            });
+
+   
+
         };
 
         function getAllBirthdays() {        
             return $q(function (resolve, reject) {
     
-                var options = {
-                    birthdays: {
-                        proto: Object,
-                        inflate: function (src, dst) {
-                            var prop;
-                            for (prop in src) {
-                                if (prop === 'Date') {
-                                    dst.Date = new Date(src.Date);
-                                } else {
-                                    dst[prop] = src[prop];
-                                }
-                            }
-                        }
-                    }
-                };
-    
-                _db.loadDatabase(options, function () {
-                    _birthdays = _db.getCollection('birthdays');
-    
-                    if (!_birthdays) {
-                        _birthdays = _db.addCollection('birthdays');
-                    }
     
                     resolve(_birthdays.data);
                 });
-            });
-        };
+            };
+        
 
         function addBirthday(birthday) {
             _birthdays.insert(birthday);
+             $rootScope.$broadcast('hanlebroadcast');
         };
 
         function updateBirthday(birthday) {            
@@ -60,12 +72,15 @@
             _birthdays.remove(birthday);
         };
 
+      
+
         return {
             initDB: initDB,
             getAllBirthdays: getAllBirthdays,
             addBirthday: addBirthday,
             updateBirthday: updateBirthday,
-            deleteBirthday: deleteBirthday
+            deleteBirthday: deleteBirthday,
+           
         };
     }
 })();
