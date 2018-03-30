5858cc5381e28d908ced0b71cc2f6cdc3dae2f10
bradcurtis <bradcurtis74@gmail.com>
2018-03-30 17:29:21 -0500

    checkin

--- angular2course/code-with-us-angular-quickstart-master/app/section-01/app.component.ts
+++ angular2course/code-with-us-angular-quickstart-master/app/section-01/app.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app', // to use me, put <my-app> in index.html
-  template: `<h1>Hello {{name}}</h1>`
+  template: `
+  <h1>Hello {{name}}</h1>
+  <fieldset>
+    <img [src]="image" />
+  </fieldset>
+  <label [style.color]="color">Favorite Color</label>
+  <button (click)="clicked()">Toggle Color</button>
+
+  `
 })
 export class AppComponent  {
   name = 'Angular';
+  image = 'favicon.ico';
+  color='red';
+
+  clicked(){
+    this.color = this.color === 'red' ? 'blue' :'red';
+  }
 }
